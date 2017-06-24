195eeb13220d158fba124ba1220635157e92e3a6
ThinhTT <ThinhTT@localhost>
2017-06-24 18:05:00 -0500

    Clean up code and add comments for some DAO class

--- SU17Team2ACMECourierServiceApplication/src/courierDAO/CourierDAO.java
+++ SU17Team2ACMECourierServiceApplication/src/courierDAO/CourierDAO.java
@@ -9,21 +9,25 @@ import courierPD.Customer;
 
 public class CourierDAO 
 {
+	// Find courier by id
 	public static Courier findCourierById(long id) 
 	{
 		return emDAO.getEM().find(Courier.class, id);
 	}
 	
+	// Save courier
 	public static void saveCourier(Courier courier) 
 	{
 		emDAO.getEM().persist(courier);
 	}
 	
+	// Add courier
 	public static void addCourier(Courier courier) 
 	{
 		emDAO.getEM().persist(courier);
 	}
 
+	// Get couriers list
 	public static List<Courier> listCourier() 
 	{
 		Query query = emDAO.getEM().createQuery("SELECT c FROM Courier c");
--- SU17Team2ACMECourierServiceApplication/src/courierDAO/UserDAO.java
+++ SU17Team2ACMECourierServiceApplication/src/courierDAO/UserDAO.java
@@ -8,46 +8,57 @@ import courierPD.User;
 
 public class UserDAO 
 {
-
-	public static void saveUser(User user) {
+	// Save a user
+	public static void saveUser(User user) 
+	{
 		emDAO.getEM().persist(user);
 	}
 	
-		public static void addUser(User user) {
-			emDAO.getEM().persist(user);
-		}
+	// Add a user
+	public static void addUser(User user) 
+	{
+		emDAO.getEM().persist(user);
+	}
 
-		public static List<User> listUser() {
-			Query query = emDAO.getEM().createQuery("SELECT u FROM user u");
-			@SuppressWarnings("unchecked")
-			List<User> list= (List<User>) query.getResultList();
+	// Get a list of all users
+	public static List<User> listUser()
+	{
+		Query query = emDAO.getEM().createQuery("SELECT u FROM user u");
+		@SuppressWarnings("unchecked")
+		List<User> list= (List<User>) query.getResultList();
 
-			return list;
-		}
+		return list;
+	}
 
-		public static User findUserById(int id) {
-			Query query = emDAO.getEM().createQuery("SELECT user FROM user user");
-			User user = (User) query.getSingleResult();
-			return user;
-		}
+	// Find a user by id
+	public static User findUserById(int id) 
+	{
+		Query query = emDAO.getEM().createQuery("SELECT user FROM user user");
+		User user = (User) query.getSingleResult();
+		return user;
+	}
 		
-		public static User findUser(String username, String password) {
-			Query query = emDAO.getEM().createQuery("SELECT u FROM user u WHERE u.userName = '" + username + "' and u.password = '" + password + "'");
-			User user = null;
-			try
-			{
-				user = (User) query.getSingleResult();
-			}
-			catch(Exception e)
-			{
-				
-			}
-
-			return user;
+	// Find a user by username and password
+	public static User findUser(String username, String password) 
+	{
+		Query query = emDAO.getEM().createQuery("SELECT u FROM user u WHERE u.userName = '" + username + "' and u.password = '" + password + "'");
+		User user = null;
+		try
+		{
+			user = (User) query.getSingleResult();
 		}
-
-		public static void removeUser(User user) {
-			emDAO.getEM().remove(user);
+		catch(Exception e)
+		{
 			
 		}
+
+		return user;
+	}
+
+	// Remove a user
+	public static void removeUser(User user) 
+	{
+		emDAO.getEM().remove(user);
+		
+	}
 }
